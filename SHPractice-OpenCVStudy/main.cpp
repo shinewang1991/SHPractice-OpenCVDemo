9007c638a7bd5e80a990f37140f78ff77d40cb2a
wangxiang <wangxiang@yixia.com>
2018-05-09 21:06:35 +0800

    Initial Commit

---
+++ SHPractice-OpenCVStudy/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  SHPractice-OpenCVStudy
+//
+//  Created by Shine on 09/05/2018.
+//  Copyright © 2018 shine. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
